11886232f1371e36b155ec84a173272a36ce967c
ziatdinovmax <ziatdinovmax@gmail.com>
2020-12-31 09:19:52 +0000

    Update import statements

---
+++ build/lib/e2cnn/nn/modules/batchnormalization/__init__.py
@@ -0,0 +1,11 @@
+from .inner import InnerBatchNorm
+from .norm import NormBatchNorm
+from .induced_norm import InducedNormBatchNorm
+from .gnorm import GNormBatchNorm
+
+__all__ = [
+    "InnerBatchNorm",
+    "NormBatchNorm",
+    "InducedNormBatchNorm",
+    "GNormBatchNorm",
+]
---
+++ build/lib/e2cnn/nn/modules/dropout/__init__.py
@@ -0,0 +1,4 @@
+from .pointwise import PointwiseDropout
+from .field import FieldDropout
+
+__all__ = ["PointwiseDropout", "FieldDropout"]
